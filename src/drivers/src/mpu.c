ac91897e2a0c3f99244ee01ade0516ac1e8e615b
Daniel Proano <179722984+DanielProano@users.noreply.github.com>
2026-07-30 23:32:49 -0400

    mpu init

---
+++ src/drivers/src/mpu.c
@@ -0,0 +1,26 @@
+#include "mpu.h"
+
+
+void mpu_init(void) {
+
+}
+
+void mpu_poll(void) {
+
+}
+
+bool mpu_get_quaternion(float *w, float *x, float *y, float *z) {
+    return true;
+}
+
+bool mpu_get_gyro(float *x, float *y, float *z) {
+    return true;
+}
+
+bool mpu_get_acceleration(float *x, float *y, float *z) {
+    return true;
+}
+
+bool mpu_get_magnitude(float *x, float *y, float *z) {
+    return true;
+}
