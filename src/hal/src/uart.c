4f8d6623be768786c233ad25fee15c959de1164c
Danny <179722984+DanielProano@users.noreply.github.com>
2026-08-16 16:21:24 -0400

    small updates

--- src/hal/src/uart.c
+++ src/hal/src/uart.c
@@ -34,13 +34,13 @@ void uart_init(void) {
     GPIOA->AFR[1] &= ~(0xF << 4);
 
     /* See page 162 of Reference Manual */
-    GPIOA->AFR[1] |= (15 << 4);
+    GPIOA->AFR[1] |= (7 << 4);
 
     /* A10 needs reset */
     GPIOA->AFR[1] &= ~(0xF << 8);
 
     /* See page 165 of Reference Manual, write AF7 into A10 */
-    GPIOA->AFR[1] |= (15 << 8);
+    GPIOA->AFR[1] |= (7 << 8);
 
     /* Configure USART1 */
 
@@ -87,3 +87,4 @@ uint8_t uart_receive_byte(void) {
 
     return (uint8_t) USART1->DR;
 }
+
