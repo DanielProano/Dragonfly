170d3bc8620adfedff615ca01fb1547e6ee9701c
Danny <179722984+DanielProano@users.noreply.github.com>
2026-09-02 22:23:04 -0400

    uart done

--- src/hal/include/uart.h
+++ src/hal/include/uart.h
@@ -1,10 +1,27 @@
 #ifndef UART_H
 #define UART_H
 
+#include <stdbool.h>
 #include <stdint.h>
 
-void uart_init(void);
-void uart_send_byte(uint8_t byte);
-uint8_t uart_receive_byte();
+typedef enum {
+    UART_1,
+    UART_2,
+    UART_6,
+    NUM_UARTS
+} uart_id_t;
+
+void uart_init(uart_id_t id, uint32_t baud);
+void uart_send_byte(uart_id_t id, uint8_t byte);
+uint8_t uart_receive_byte(uart_id_t id);
+void uart_send_bytes(uart_id_t id, const uint8_t *buffer, uint32_t length);
+void uart_receive_bytes(uart_id_t id, uint8_t *buffer, uint32_t length);
+bool uart_receive_byte_timeout(uart_id_t id, uint8_t *byte, uint32_t timeout_ms);
+bool uart_receive_bytes_timeout(uart_id_t id, uint8_t *buffer, uint32_t length, uint32_t timeout_ms);
+uint32_t uart_rx_overrun_count(uart_id_t id);
+
+void USART1_IRQHandler(void);
+void USART2_IRQHandler(void);
+void USART6_IRQHandler(void);
 
 #endif
--- src/hal/src/uart.c
+++ src/hal/src/uart.c
@@ -1,90 +1,245 @@
 #include "uart.h"
 #include "stm32f401xc.h"
-#include <stdio.h>
+#include "queue.h"
+#include "mutex.h"
+#include "scheduler.h"
 
-void uart_init(void) {
+#define UART_TX_QUEUE_DEPTH 64u
+#define UART_RX_QUEUE_DEPTH 64u
+
+typedef struct {
+    USART_TypeDef *base;
+    IRQn_Type irqn;
+    uint8_t tx_buffer[UART_TX_QUEUE_DEPTH];
+    uint8_t rx_buffer[UART_RX_QUEUE_DEPTH];
+    Queue tx_queue;
+    Queue rx_queue;
+    Mutex tx_mutex;
+    Mutex rx_mutex;
+    volatile uint32_t rx_overrun_count;
+} uart_instance_t;
+
+static uart_instance_t uart_instances[NUM_UARTS];
+
+void uart_init(uart_id_t id, uint32_t baud) {
+    uart_instance_t *instance = &uart_instances[id];
+    uint32_t pclk_hz;
+
     /* Page 119 of Reference */
 
     /* Turn on GPIOA pin clock */
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
 
-    /* Page 123 of Reference Manual */
+    switch (id) {
+        case UART_1:
+            /* Page 123 of Reference Manual */
 
-    /* Enable USART1 clock */
-    RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
+            /* Enable USART1 clock */
+            RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
 
-    /* Page 158 in Reference Manual*/
+            /* Page 158 in Reference Manual*/
 
-    /* Clear GPIO A9 Pin */
-    GPIOA->MODER &= ~(0x3 << 18);
-    /* Set A9 to alternate function mode (USART1_TX) */
-    GPIOA->MODER |= 0x2 << 18;
+            /* Clear GPIO A9 Pin */
+            GPIOA->MODER &= ~(0x3U << 18);
+            /* Set A9 to alternate function mode (USART1_TX) */
+            GPIOA->MODER |= (0x2U << 18);
 
-    /* Clear GPIO A10 Pin*/
-    GPIOA->MODER &= ~(0x3 << 20);
-    /* Set A10 to alternate function mode (USART1_RX) */
-    GPIOA->MODER |= 0x2 << 20;
+            /* Clear GPIO A10 Pin*/
+            GPIOA->MODER &= ~(0x3U << 20);
+            /* Set A10 to alternate function mode (USART1_RX) */
+            GPIOA->MODER |= (0x2U << 20);
 
-    /*  Configure alternate function registers */
-    /*  Page 44 of STM32 datasheet demands we write
-        AF07 into A9 & A10 to configure alternate
-        function mapping */
+            /*  Configure alternate function registers */
+            /*  Page 44 of STM32 datasheet demands we write
+                AF07 into A9 & A10 to configure alternate
+                function mapping */
 
-    /* We need to configure pin 9 for A9, reset it*/
-    GPIOA->AFR[1] &= ~(0xF << 4);
+            /* We need to configure pin 9 for A9, reset it*/
+            GPIOA->AFR[1] &= ~(0xFU << 4);
 
-    /* See page 162 of Reference Manual */
-    GPIOA->AFR[1] |= (7 << 4);
+            /* See page 162 of Reference Manual */
+            GPIOA->AFR[1] |= (7U << 4);
 
-    /* A10 needs reset */
-    GPIOA->AFR[1] &= ~(0xF << 8);
+            /* A10 needs reset */
+            GPIOA->AFR[1] &= ~(0xFU << 8);
 
-    /* See page 165 of Reference Manual, write AF7 into A10 */
-    GPIOA->AFR[1] |= (7 << 8);
+            /* See page 165 of Reference Manual, write AF7 into A10 */
+            GPIOA->AFR[1] |= (7U << 8);
 
-    /* Configure USART1 */
+            instance->base = USART1;
+            instance->irqn = USART1_IRQn;
+            pclk_hz = 84000000U;
+            break;
 
-    /*  Baud rate (data transfer speed) is 
-        configured to 0, so need to set it 
-        manually */
-    
+        case UART_2:
+            RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
+
+            GPIOA->MODER &= ~(0x3U << 4);
+            GPIOA->MODER |= (0x2U << 4);
+            GPIOA->MODER &= ~(0x3U << 6);
+            GPIOA->MODER |= (0x2U << 6);
+
+            GPIOA->AFR[0] &= ~(0xFU << 8);
+            GPIOA->AFR[0] |= (7U << 8);
+            GPIOA->AFR[0] &= ~(0xFU << 12);
+            GPIOA->AFR[0] |= (7U << 12);
+
+            instance->base = USART2;
+            instance->irqn = USART2_IRQn;
+            pclk_hz = 42000000U;
+            break;
+
+        case UART_6:
+            RCC->APB2ENR |= RCC_APB2ENR_USART6EN;
+
+            GPIOA->MODER &= ~(0x3U << 22);
+            GPIOA->MODER |= (0x2U << 22);
+            GPIOA->MODER &= ~(0x3U << 24);
+            GPIOA->MODER |= (0x2U << 24);
+
+            GPIOA->AFR[1] &= ~(0xFU << 12);
+            GPIOA->AFR[1] |= (8U << 12);
+            GPIOA->AFR[1] &= ~(0xFU << 16);
+            GPIOA->AFR[1] |= (8U << 16);
+
+            instance->base = USART6;
+            instance->irqn = USART6_IRQn;
+            pclk_hz = 84000000U;
+            break;
+
+        default:
+            return;
+    }
+
     /*  Page 552 of Reference Manual
-        Most common baud rate is 115,200, 
         Baud Rate = f_CK / ( 8 * (2 - OVER8) * USARTDIV)
-        Default is 0 for control register, meaning 
+        Default is 0 for control register, meaning
         OVER8 is 0, meaning oversamping by 16
-        = f_CK / ( 16 * USARTDIV) 
+        = f_CK / ( 16 * USARTDIV)
         Rearrange for USARTDIV:
         USARTDIV = f_CK / (Baud Rate * 16 )
-        Substitute:
-        USARTDIV = 84,000,000 / (115,200 * 16) = 45.572
-        Now put into mantissa & fractional form:
-        Mantissa needs to represent 45
-        Need to get .572 into 1/16s form:
-        ? / 16 = .572; ? = .572 * 16 = ~9 
-        So we get (45 << 4) | 9 */
-    USART1->BRR = ((45 << 4) | 9);
+        USARTDIV * 16 = f_CK / Baud Rate
+        Put USARTDIV * 16 into mantissa & fractional form:
+        mantissa = (USARTDIV * 16) / 16
+        fraction = (USARTDIV * 16) % 16 */
+    uint32_t usartdiv_x16 = (pclk_hz + (baud / 2U)) / baud;
+    instance->base->BRR = ((usartdiv_x16 / 16U) << 4) | (usartdiv_x16 % 16U);
 
     /* Now enable Transmitter*/
-    USART1->CR1 |= USART_CR1_TE;
+    instance->base->CR1 |= USART_CR1_TE;
     /* Enable Receiver */
-    USART1->CR1 |= USART_CR1_RE;
+    instance->base->CR1 |= USART_CR1_RE;
     /* Enable USART */
-    USART1->CR1 |= USART_CR1_UE;
+    instance->base->CR1 |= USART_CR1_UE;
+
+    queue_init(&instance->tx_queue, instance->tx_buffer, sizeof(uint8_t), UART_TX_QUEUE_DEPTH);
+    queue_init(&instance->rx_queue, instance->rx_buffer, sizeof(uint8_t), UART_RX_QUEUE_DEPTH);
+    mutex_init(&instance->tx_mutex);
+    mutex_init(&instance->rx_mutex);
+    instance->rx_overrun_count = 0;
+
+    instance->base->CR1 |= USART_CR1_RXNEIE;
+    NVIC_EnableIRQ(instance->irqn);
 }
 
-void uart_send_byte(uint8_t byte) {
-    /* check if data was sent to shift register yet */
-    while (!(USART1->SR & USART_SR_TXE));
-
-    /* Now write byte into data register */
-    USART1->DR = byte;
+void uart_send_byte(uart_id_t id, uint8_t byte) {
+    uart_send_bytes(id, &byte, 1);
 }
 
-uint8_t uart_receive_byte(void) {
-    /* Wait for data register to be ready */
-    while (!(USART1->SR &  USART_SR_RXNE));
-
-    return (uint8_t) USART1->DR;
+uint8_t uart_receive_byte(uart_id_t id) {
+    uint8_t byte;
+    uart_receive_bytes(id, &byte, 1);
+    return byte;
 }
 
+void uart_send_bytes(uart_id_t id, const uint8_t *buffer, uint32_t length) {
+    uart_instance_t *instance = &uart_instances[id];
+
+    mutex_lock(&instance->tx_mutex);
+
+    for (uint32_t i = 0; i < length; i++) {
+        enqueue(&instance->tx_queue, (void *) &buffer[i]);
+    }
+
+    mutex_unlock(&instance->tx_mutex);
+
+    instance->base->CR1 |= USART_CR1_TXEIE;
+}
+
+void uart_receive_bytes(uart_id_t id, uint8_t *buffer, uint32_t length) {
+    uart_instance_t *instance = &uart_instances[id];
+
+    mutex_lock(&instance->rx_mutex);
+
+    for (uint32_t i = 0; i < length; i++) {
+        dequeue(&instance->rx_queue, &buffer[i]);
+    }
+
+    mutex_unlock(&instance->rx_mutex);
+}
+
+bool uart_receive_byte_timeout(uart_id_t id, uint8_t *byte, uint32_t timeout_ms) {
+    return uart_receive_bytes_timeout(id, byte, 1, timeout_ms);
+}
+
+bool uart_receive_bytes_timeout(uart_id_t id, uint8_t *buffer, uint32_t length, uint32_t timeout_ms) {
+    uart_instance_t *instance = &uart_instances[id];
+    uint32_t deadline = scheduler_get_tick_count() + timeout_ms;
+    bool success = true;
+
+    mutex_lock(&instance->rx_mutex);
+
+    for (uint32_t i = 0; i < length; i++) {
+        uint32_t now = scheduler_get_tick_count();
+        uint32_t remaining = (deadline > now) ? (deadline - now) : 0;
+
+        if (!dequeue_timeout(&instance->rx_queue, &buffer[i], remaining)) {
+            success = false;
+            break;
+        }
+    }
+
+    mutex_unlock(&instance->rx_mutex);
+
+    return success;
+}
+
+uint32_t uart_rx_overrun_count(uart_id_t id) {
+    return uart_instances[id].rx_overrun_count;
+}
+
+static void uart_irq_common(uart_instance_t *instance) {
+    uint32_t sr = instance->base->SR;
+
+    if (sr & (USART_SR_RXNE | USART_SR_ORE | USART_SR_FE | USART_SR_NE)) {
+        uint8_t byte = (uint8_t) instance->base->DR;
+
+        if ((sr & USART_SR_RXNE) && !(sr & (USART_SR_ORE | USART_SR_FE | USART_SR_NE))) {
+            if (!enqueue_isr(&instance->rx_queue, &byte)) {
+                instance->rx_overrun_count += 1;
+            }
+        }
+    }
+
+    if (sr & USART_SR_TXE) {
+        uint8_t byte;
+
+        if (dequeue_isr(&instance->tx_queue, &byte)) {
+            instance->base->DR = byte;
+        } else {
+            instance->base->CR1 &= ~USART_CR1_TXEIE;
+        }
+    }
+}
+
+void USART1_IRQHandler(void) {
+    uart_irq_common(&uart_instances[UART_1]);
+}
+
+void USART2_IRQHandler(void) {
+    uart_irq_common(&uart_instances[UART_2]);
+}
+
+void USART6_IRQHandler(void) {
+    uart_irq_common(&uart_instances[UART_6]);
+}
